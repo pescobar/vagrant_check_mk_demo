6b92619157ed20e146e6d7c59321e5166196a48b
Pablo Escobar <pescobar001@gmail.com>
2018-09-04 17:22:32 +0200

    change password before starting the service

--- install_checkmk.sh
+++ install_checkmk.sh
@@ -5,9 +5,8 @@ sudo yum -y install epel-release
 wget https://mathias-kettner.de/support/1.5.0p3/check-mk-raw-1.5.0p3-el7-38.x86_64.rpm
 sudo yum -y localinstall check-mk-raw-1.5.0p3-el7-38.x86_64.rpm
 sudo omd create mysite
-sudo omd start mysite
 echo cmkadmin | htpasswd -i -m /omd/sites/mysite/etc/htpasswd cmkadmin
-sudo omd restart
+sudo omd start mysite
 
 # install the check_mk agent
 wget http://192.168.33.10/mysite/check_mk/agents/check-mk-agent-1.5.0p3-1.noarch.rpm
